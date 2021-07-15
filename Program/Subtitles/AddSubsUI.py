ce5dd526817458cdc76bd92d2ba5025aad27745d
Stephen Caesar <stephen.caesar@hotmail.co.uk>
2021-07-15 19:34:35 +0100

    Added guide for adding video and subtitle files

--- Program/Subtitles/AddSubsUI.py
+++ Program/Subtitles/AddSubsUI.py
@@ -3,9 +3,23 @@
 # UI for adding subtitle files to the user files
 
 import PySimpleGUI as sg
+import os
 
 def addScreen():
-    text = [[sg.Text('placeholder window')]]
+    startPath = os.getcwd().split('\\')
+    startPath = startPath[:len(startPath)-2]
+    startPath = '\\'.join(startPath) + '\\Program\\Subtitles\\'
+       
+    text = [[sg.Text('Navigate to the \'Subtitles\' file, and create a folder for the content')],
+            [sg.Image(startPath + 'addSubs_1.png')],
+            [sg.Text('')],
+            [sg.Text('Move the video files (.mp4 or .mkv) and subtitle files (.srt) to the folder')],
+            [sg.Image(startPath + 'addSubs_2.png')],
+            [sg.Text('')],
+            [sg.Text('Make sure the matching video and subtitle files have the same name')],
+            [sg.Image(startPath + 'addSubs_3.png')],
+            [sg.Text('')],
+            [sg.Button('Back')]]
 
     addScreen = [[sg.Column(text)]]
 
@@ -18,7 +32,9 @@ def addSubs():
     while True:
         event, values = wAddScreen.read()
         
-        if event == None or event == 'Exit':
+        if event == None or event == 'Exit' or event == 'Back':
             break
+        
+    wAddScreen.Close()
 
     return
