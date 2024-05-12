28e6042161fd86d6cc960d71dc20bf3b0f10c43a
Stephen Caesar <stephen.caesar@hotmail.co.uk>
2024-05-12 13:54:35 +0100

    Class was not supposed to be inheriting Event

--- subscope/retime/retime_state.py
+++ subscope/retime/retime_state.py
@@ -1,7 +1,4 @@
-from subscope.shared.event import Event
-
-
-class RetimeState(Event):
+class RetimeState:
     def __init__(self, theme=None, folder=None, files=None, selected_files=None):
         self.theme = theme
         self.folder = folder
